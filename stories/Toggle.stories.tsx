0b76200bd0cedd3cb86e750a224900408cf32fa4
cwong6854 <curtiswong9990@gmail.com>
2024-12-20 12:32:20 -0800

    add toggle to storybook

---
+++ stories/Toggle.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { Toggle } from '../components/ui/toggle';
+import { VariantProps } from 'class-variance-authority';
+
+export default {
+  title: 'Components/Toggle',
+  component: Toggle,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: ['default', 'outline'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['default', 'sm', 'lg'],
+      },
+    },
+    onClick: { action: 'clicked' },
+  },
+} as Meta;
+
+const Template: StoryFn<VariantProps<typeof Toggle>> = (args) => <Toggle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Default Button',
+  variant: 'default',
+  size: 'default'
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  children: 'Outline',
+  variant: 'outline',
+};
+
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Secondary Button',
+  pressed: false,
+  disabled: true
+};
