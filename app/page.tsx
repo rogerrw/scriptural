d2f720c9ebb72c9419d0e384270f478dec67bb58
Roger Wang <roger.wang.r@gmail.com>
2024-12-21 15:45:01 -0800

    autoformat

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import RootLayout from './layout'
+import React from 'react';
+import RootLayout from './layout';
 
 const HomePage = () => {
   return (
     <RootLayout>
       <div>HomePage</div>
     </RootLayout>
-  )
-}
+  );
+};
 
 export default HomePage;
