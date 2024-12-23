c59f1655fc10c851ae2b6dd5397fe7392b4d2783
cwong6854 <curtiswong9990@gmail.com>
2024-12-22 16:07:27 -0800

    label, input & popover storybook

---
+++ stories/Input.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { Input } from '@/components/ui/input';
+import { VariantProps } from 'class-variance-authority';
+
+export default {
+    title: 'Components/Input',
+    component: Input,
+  } as Meta;
+
+const Template: StoryFn<VariantProps<typeof Input>> = (args) => <Input {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    type: 'email',
+    placeholder: 'Email'
+};
+
+export const File = Template.bind({});
+File.args = {
+    type: 'file',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    disabled: true,
+    type: 'email',
+    placeholder: 'Email'
+};
