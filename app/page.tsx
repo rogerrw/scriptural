b1c81c71f5d53b4da32e37ff8309f5ee5a7a282a
Roger Wang <roger.wang.r@gmail.com>
2025-01-17 11:18:59 -0800

    add completion logic for verse typer, hide input and only show verse display with ability to autofocus the input on clicking the display

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <VerseTyper verseText={`Your word is a lamp to my feet and a light to my path.`} />
+      <VerseTyper
+        verseText={`Then I saw thrones, and seated on them were those to whom the authority to judge was committed. Also I saw the souls of those who had been beheaded for the testimony of Jesus and for the word of God, and those who had not worshiped the beast or its image and had not received its mark on their foreheads or their hands. They came to life and reigned with Christ for a thousand years.`}
+      />
     </div>
   );
 };
--- component-library/verseTyper/index.tsx
+++ component-library/verseTyper/index.tsx
@@ -4,6 +4,10 @@ import { Input } from '../input';
 
 interface VerseTyperProps {
   verseText: string;
+  /**
+   * Difficulty level of the verse being typed. At increasing difficulty levels, more words will be hidden until none of the reference verse shows up. The user will also be awarded more points at higher levels of difficulty
+   */
+  difficulty?: 0 | 1 | 2 | 3 | 4 | 5;
 }
 
 interface WordProps {
@@ -18,11 +22,11 @@ interface LetterProps {
 
 const Letter = ({ referenceChar, typedChar }: LetterProps) => {
   if (!typedChar?.length) {
-    return <div className="text-slate-600 opacity-0">{referenceChar}</div>;
+    return <div className="text-slate-600">{referenceChar}</div>;
   }
 
   if (typedChar.toLowerCase() !== referenceChar.toLowerCase()) {
-    return <div className="text-red-400">{typedChar}</div>;
+    return <div className="text-red-400 line-through">{typedChar}</div>;
   }
   return <div className="text-white">{typedChar}</div>;
 };
@@ -45,33 +49,59 @@ const Word = ({ referenceWord, typedWord, validationMethod = 'alphanumeric' }: W
   );
 };
 
-const VerseTyper = ({ verseText }: VerseTyperProps) => {
+const VerseTyper = ({ verseText, difficulty = 0 }: VerseTyperProps) => {
   const [typedText, setTypedText] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const verseWords = verseText.split(' ');
+  const typedWords = typedText.split(' ');
+
+  const handleComplete = () => {
+    // TODO: Trigger review summary
+    console.log('Verse complete!');
+  };
 
   const renderText = () => {
     return (
       // Add onfocus and onblur handlers for this div to focus/blur the hidden text input
-      <div className="flex justify-evenly">
-        {verseText
-          .toLowerCase()
-          .split(' ')
-          .map((word, index) => {
-            return <Word referenceWord={word} typedWord={typedText.split(' ')?.[index]} />;
-          })}
+      <div
+        id="verse-display"
+        className="mx-32 flex flex-wrap gap-8 font-mono text-4xl"
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+      >
+        {verseWords.map((word, index) => {
+          return <Word referenceWord={word} typedWord={typedText.split(' ')?.[index]} />;
+        })}
       </div>
     );
   };
 
   return (
     <div>
+      {renderText()}
       <Input
+        ref={inputRef}
+        autoFocus
         onChange={(e) => {
-          setTypedText(e.target.value);
+          const newlyTypedText = e.target.value;
+          setTypedText(newlyTypedText);
+
+          // check if complete
+          const newlyTypedWords = newlyTypedText.toLowerCase().split(' ');
+          const finalTypedWord = newlyTypedWords[verseWords.length - 1];
+          const finalVerseWord = verseWords[verseWords.length - 1];
+          const isComplete =
+            newlyTypedWords.length === verseWords.length && finalTypedWord === finalVerseWord;
+
+          if (isComplete) {
+            handleComplete();
+          }
         }}
         value={typedText}
+        className="absolute h-0 w-0 opacity-0"
       />
-      {verseText}
-      {renderText()}
     </div>
   );
 };
