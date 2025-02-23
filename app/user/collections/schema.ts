8dac6206e988189ffe715faf75cdd298875a6793
cwong6854 <curtiswong9990@gmail.com>
2025-02-22 23:02:07 -0800

    fix from email to string zod

--- app/user/collections/schema.ts
+++ app/user/collections/schema.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 export const CollectionSchema = z.object({
-  userId: z.string().email({
+  userId: z.string({
     message: 'User id is required',
   }),
   name: z.string(),
