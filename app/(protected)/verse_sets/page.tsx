3f80c936694d9f1c888cbef5a2ec083228da2619
Roger Wang <roger.wang.r@gmail.com>
2025-04-24 21:57:48 -0700

    add dropdown menu to component library, update header to match designs, add review, search and verse_sets pages

---
+++ app/(protected)/verse_sets/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VerseSetsPage = () => {
+  return <div>VerseSetsPage</div>;
+};
+
+export default VerseSetsPage;
--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,16 @@
 import { Button } from '@/component-library/button';
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Calendar1Icon, ListIcon, LogOutIcon, SearchIcon, UserIcon } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from '@/component-library/dropdown-menu';
+import { DropdownMenuTrigger } from '@/component-library/dropdown-menu';
 
 const Header = async () => {
   const session = await auth();
@@ -15,20 +24,52 @@ const Header = async () => {
 
   const renderUserAccount = () => {
     return (
-      <Button variant="ghost" asChild>
-        <Link href="/dashboard" className="flex items-center gap-2">
-          {session?.user?.name}
-        </Link>
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost">
+            <UserIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            <Link href="/dashboard">Account</Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <>
+              <LogOutIcon />
+              <Link href="/auth/logout">Logout</Link>
+            </>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     );
   };
 
   return (
-    <header className="fadein flex items-center justify-between p-8">
+    <header className="fadein mx-8 flex items-center justify-between p-8">
       <Link href="/">
         <Image src="/sample_logo.png" width={240} height={40} alt="Picture of the author" />
       </Link>
-      <div>{!!session ? renderUserAccount() : renderLoginButton()}</div>
+      <div className="flex items-center gap-2">
+        <Button asChild>
+          <Link href="/review" className="flex items-center gap-2">
+            <Calendar1Icon className="h-4 w-4" />
+            Daily Review
+          </Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/search" className="flex items-center gap-2">
+            <SearchIcon className="h-4 w-4" />
+          </Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/verse_sets" className="flex items-center gap-2">
+            <ListIcon className="h-4 w-4" />
+          </Link>
+        </Button>
+        {!!session ? renderUserAccount() : renderLoginButton()}
+      </div>
     </header>
   );
 };
