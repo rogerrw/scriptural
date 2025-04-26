4c037ef7002168c2f4dd76f65e48bb2ae78a2cc5
Roger Wang <roger.wang.r@gmail.com>
2025-04-26 13:29:33 -0700

    add separator component, add ui for verse sets left menu

--- app/(protected)/verse_sets/page.tsx
+++ app/(protected)/verse_sets/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { ScrollArea } from '@/component-library/scroll-area';
 import { Card } from '@/component-library/card';
 import { Button } from '@/component-library/button';
+import { Separator } from '@/component-library/separator';
+import { PlusIcon } from 'lucide-react';
+
 const VerseSetsPage = () => {
   const sampleVerseSets = [
     {
@@ -20,12 +23,29 @@ const VerseSetsPage = () => {
 
   return (
     <div className="fadein flex flex-row gap-4">
-      <Card className="w-1/2">
-        <ScrollArea>
-          {sampleVerseSets.map((verseSet) => (
-            <div key={verseSet.id}>
-              <Button variant="link">{verseSet.name}</Button>
-            </div>
+      <Card className="h-full w-80 max-w-80">
+        <div className="flex items-center justify-between bg-gray-800 p-4">
+          <span className="ml-2 font-bold text-gray-900 dark:text-gray-300">Verse Sets</span>
+          <Button
+            variant="ghost"
+            className="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:bg-gray-700"
+          >
+            <PlusIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        <ScrollArea className="h-full w-full p-2">
+          {sampleVerseSets.map((verseSet, index) => (
+            <React.Fragment key={verseSet.id}>
+              <Button
+                className="my-1 w-full text-left text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700"
+                variant="ghost"
+              >
+                {verseSet.name}
+              </Button>
+              {index !== sampleVerseSets.length - 1 && (
+                <Separator className="bg-gray-200 dark:bg-gray-700" />
+              )}
+            </React.Fragment>
           ))}
         </ScrollArea>
       </Card>
