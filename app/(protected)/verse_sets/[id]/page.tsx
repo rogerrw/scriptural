78bd6494c13ce7c0d0f55b4a148d3e6409ba357a
Roger Wang <roger.wang.r@gmail.com>
2025-06-14 19:26:00 -0700

    style verse card in verse set view

--- app/(protected)/verse_sets/[id]/page.tsx
+++ app/(protected)/verse_sets/[id]/page.tsx
@@ -2,9 +2,16 @@ import { fetchVerseSet } from '@/actions/fetchVerseSet';
 import { Button } from '@/component-library/button';
 import { KeyboardIcon } from 'lucide-react';
 import AddVerseDialog from './AddVerseDialog';
+import VerseCard from '@/component-library/verse-card';
 
 const VerseSetPage = async ({ params }: { params: { id: string } }) => {
   const verseSet = await fetchVerseSet(params.id);
+
+  if (verseSet.error) {
+    return <div>{verseSet.error}</div>;
+  }
+
+  const verses = verseSet?.verses;
   return (
     <div className="fadein flex flex-col gap-8 px-12 py-4">
       <h1 className="text-3xl font-bold">{verseSet?.name}</h1>
@@ -14,6 +21,18 @@ const VerseSetPage = async ({ params }: { params: { id: string } }) => {
           Review
         </Button>
         <AddVerseDialog verseSetId={params.id} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {verses?.map((verse) => (
+          <VerseCard
+            key={verse.id}
+            verseText={verse.userVerse.verseText}
+            book={verse.userVerse.book}
+            chapterNumber={verse.userVerse.chapter}
+            verseNumber={verse.userVerse.startingVerse}
+            translation={verse.userVerse.translation}
+          />
+        ))}
       </div>
     </div>
   );
